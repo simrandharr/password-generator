1014895b7372fa255e75c12178ca17b1d3d9a690
simrandharr <simrandhar62@gmail.com>
2025-10-02 16:42:41 +0530

    Add password generator code

---
+++ password.py
@@ -0,0 +1,9 @@
+import random
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
+length = int(input("Enter password length: "))
+password = ""
+for i in range(length):
+    password += random.choice(chars)
+
+print("Your password is:", password)
